831bc6064c8abc5466a557907f7d278123f55ad3
Jay Hansea <ezeokafortochukwu@gmail.com>
2023-08-06 12:55:58 +0100

    started creating the task app on App.js

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Just to test it's properly working</Text>
-      <StatusBar style="auto" />
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +18,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e8eaed',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {},
 });
